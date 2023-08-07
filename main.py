3f1b8be7bdd3c1f09db5733d9848c22baf7f2e82
HaritsAcheiz <harits.muhammad.only@gmail.com>
2023-08-08 04:47:53 +0700

    try to store scraped data in google spreadsheet using API

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pandas as pd
 from scraper import InsightScraper
 
 if __name__ == '__main__':
-    # s = InsightScraper()
-    # product_df = s.run()
-    # product_df.to_csv('products.csv', index=False)
+    s = InsightScraper()
+    product_df = s.run()
+    product_df.to_csv('products.csv', index=False)
     product_df = pd.read_csv('products.csv')
     product_df.info()
--- scraper.py
+++ scraper.py
@@ -1,8 +1,14 @@
 import json
 import httpx
+from googleapiclient.errors import HttpError
 from selectolax.parser import HTMLParser
 from dataclasses import dataclass
 import pandas as pd
+from googleapiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+from google_auth_oauthlib.flow import InstalledAppFlow,Flow
+from google.auth.transport.requests import Request
+
 
 @dataclass
 class InsightScraper:
@@ -31,8 +37,58 @@ class InsightScraper:
 
         return df
 
+    def create_service(self):
+        scopes = ['https://www.googleapis.com/auth/spreadsheets']
+        with open('products-spreadsheet-395219-554760172bff.json') as f:
+            token = json.load(f)
+        print(token)
+        creds = ServiceAccountCredentials.from_json_keyfile_dict(token,scopes=scopes)
+        print(creds)
+        service = build('sheets', 'v4', credentials=creds)
+
+        return service
+
+    def create_sheet(self, service, title):
+        try:
+            spreadsheet = {
+                'properties': {
+                    'title': title
+                }
+            }
+            spreadsheet = service.spreadsheets().create(body=spreadsheet,fields='spreadsheetId').execute()
+            print(f"Spreadsheet ID: {(spreadsheet.get('spreadsheetId'))}")
+
+            return spreadsheet.get('spreadsheetId')
+
+        except HttpError as error:
+            print(f"An error occurred: {error}")
+
+            return error
+
+    def get_values(self, service, spreadsheet_id, range_name):
+        try:
+            result = service.spreadsheets().values().get(
+                spreadsheetId=spreadsheet_id, range=range_name).execute()
+            rows = result.get('values', [])
+            print(f"{len(rows)} rows retrieved")
+
+            return result
+
+        except HttpError as error:
+            print(f"An error occurred: {error}")
+
+            return error
+
+    def update_values(self,service, spreadsheet_id, range_name, ):
+
+    def to_google_sheet(self):
+        pass
+
     def run(self):
-        return self.get_products()
+        service = self.create_service()
+        # result = self.create_sheet(service, title='first_try')
+        self.get_values(service, spreadsheet_id='13JjE2R_vvwrPiMapF1g2m0Whrkdeci_rI7Y-osqGqlY', range_name="A1:C2")
+        # return self.get_products()
 
 
 if __name__ == '__main__':
